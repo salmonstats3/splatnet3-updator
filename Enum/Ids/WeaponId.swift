1380c03611b904e7e46c57e0edf4a6e5402509bd
tkgstrator <nasawake.am@gmail.com>
2023-03-08 00:56:27 +0000

    SplatNet3 Updator

--- Enum/Ids/WeaponId.swift
+++ Enum/Ids/WeaponId.swift
@@ -1,7 +1,7 @@
 //
 //  WeaponId.swift
 //
-//  Created by tkgstrator on 2023/03/07
+//  Created by tkgstrator on 2023/03/08
 //  Copyright @2022 Magi, Corporation. All rights reserved.
 //
 
--- Enum/Keys/BadgeKey.swift
+++ Enum/Keys/BadgeKey.swift
@@ -1,7 +1,7 @@
 //
 //  BadgeKey.swift
 //
-//  Created by tkgstrator on 2023/03/07
+//  Created by tkgstrator on 2023/03/08
 //  Copyright @2022 Magi, Corporation. All rights reserved.
 //
 
