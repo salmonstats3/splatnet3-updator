05b6c03c252d320ba2b168a63dcdd553aed7363c
tkgstrator <nasawake.am@gmail.com>
2023-05-13 00:44:21 +0000

    SplatNet3 Updator

--- Enum/Ids/NameplateId.swift
+++ Enum/Ids/NameplateId.swift
@@ -1,7 +1,7 @@
 //
 //  NameplateId.swift
 //
-//  Created by tkgstrator on 2023/05/12
+//  Created by tkgstrator on 2023/05/13
 //  Copyright @2022 Magi, Corporation. All rights reserved.
 //
 
--- Enum/Keys/BadgeKey.swift
+++ Enum/Keys/BadgeKey.swift
@@ -1,7 +1,7 @@
 //
 //  BadgeKey.swift
 //
-//  Created by tkgstrator on 2023/05/12
+//  Created by tkgstrator on 2023/05/13
 //  Copyright @2022 Magi, Corporation. All rights reserved.
 //
 
