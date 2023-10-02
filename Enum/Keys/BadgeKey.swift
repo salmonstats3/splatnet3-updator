8ba14f1caeb6a1e4a76b99c1558a548eaff55c9d
tkgstrator <nasawake.am@gmail.com>
2023-10-02 00:41:55 +0000

    SplatNet3 Updator

--- Enum/Keys/BadgeKey.swift
+++ Enum/Keys/BadgeKey.swift
@@ -1,7 +1,7 @@
 //
 //  BadgeKey.swift
 //
-//  Created by tkgstrator on 2023/10/01
+//  Created by tkgstrator on 2023/10/02
 //  Copyright @2022 Magi, Corporation. All rights reserved.
 //
 
--- Enum/LocalizedType.swift
+++ Enum/LocalizedType.swift
@@ -1,7 +1,7 @@
 //
 //  LocalizedType.swift
 //
-//  Created by tkgstrator on 2023/10/01
+//  Created by tkgstrator on 2023/10/02
 //  Copyright @2022 Magi, Corporation. All rights reserved.
 //
 
